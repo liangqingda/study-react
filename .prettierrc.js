44f25663b39355870bc240c2931013dbf612de10
liangqingda <2120927791@qq.com>
2026-05-19 09:00:43 +0800

    update

---
+++ .prettierrc.js
@@ -0,0 +1,1 @@
+module.exports = require("@liangqingda/eslint-config/prettier.json");
--- eslint.config.js
+++ eslint.config.js
@@ -1,57 +1,14 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import importPlugin from 'eslint-plugin-import'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import reactTypedConfig from "@liangqingda/eslint-config/react-typed";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
+  ...reactTypedConfig,
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      importPlugin.flatConfigs.recommended,
-      importPlugin.flatConfigs.typescript,
-    ],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    settings: {
-      'import/resolver': {
-        node: true,
-        typescript: true,
-      },
-    },
+    ignores: ["dist/**", "eslint.config.js", ".prettierrc.js"],
+  },
+  {
+    files: ["**/*.d.ts"],
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'import/order': [
-        'error',
-        {
-          groups: [
-            'builtin',
-            'external',
-            'internal',
-            ['parent', 'sibling', 'index'],
-            'type',
-          ],
-          'newlines-between': 'always',
-          alphabetize: {
-            order: 'asc',
-            caseInsensitive: true,
-          },
-        },
-      ],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      "spaced-comment": "off",
     },
   },
-)
+];
