8c03ae028bea1b762e819d32f259d54fe46c86ca
liangqingda <2120927791@qq.com>
2026-05-21 20:08:19 +0800

    1

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,9 @@
-import reactTypedConfig from "@liangqingda/eslint-config/react-typed";
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import reactTypedConfig from '@liangqingda/eslint-config/react-typed';
+
+const rootDir = dirname(fileURLToPath(import.meta.url));
 
 export default [
   ...reactTypedConfig,
@@ -6,10 +11,20 @@ export default [
     ignores: ["dist/**", "eslint.config.js", ".prettierrc.js"],
   },
   {
-    files: ["**/*.d.ts"],
+    files: ['vite.config.ts'],
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.node.json'],
+        projectService: false,
+        tsconfigRootDir: rootDir,
+      },
+    },
+  },
+  {
+    files: ['**/*.d.ts'],
     rules: {
       // .d.ts 文件不检查注释前后空格格式
-      "spaced-comment": "off",
+      'spaced-comment': 'off',
     },
   },
 ];
